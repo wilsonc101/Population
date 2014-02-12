40565cc26372c51c3daa84ea5412be785bcef73e
wilsonc101 <wilsonc101@gmail.com>
2014-02-12 23:30:10 +0000

    matching working

--- unit/update.py
+++ unit/update.py
@@ -23,40 +23,40 @@ def _Match(collection_births, collection_matches):
 	# Match values
 	matches_per_iteration = 5	# number of matches to make per iteration
 	max_age_gap = 5			# age gap between matched units
-	offspring = 0			# offspring generated from match
-			
+	max_subunits = 0		# offspring generated from match
 
 
 	matches = 0
 	while matches < matches_per_iteration:
 		matches += 1
-		
-		
-
-
 
 		# Select first partner in match
-		unitA = collection_births.find_one({"matched" : 0, "gender" : "m", "age" : {"$gte" : 20}})
+		unitA = collection_births.find_one({"matched" : 0, "gender" : "m", "age" : {"$gte" : 20}}) 
 
-		# Select second partner in match
-		unitB = collection_births.find_one({"matched" : 0, "gender" : "f", "age" : {"$gte" : 20, "$gte" : unitA["age"]-5, "$lte" : unitA["age"]+5}})
+
+		# Select second partner in match if fisrt is present
+		if (unitA != None):
+			unitB = collection_births.find_one({"matched" : 0, "gender" : "f", "age" : {"$gte" : 20, "$gte" : unitA["age"]-5, "$lte" : unitA["age"]+5}})
+
 		
-		try:
-			print(unitA["age"])
-			print(unitB["age"])
-			print("units joined - test")
-		except:
-			print("nothing to join")
+		# If both units are present, mark as matched and record match
+		if (unitA != None and unitB != None):		
+			collection_births.update({"_id" : unitA["_id"]},{"$set":{"matched" : 1}}, upsert=False, multi=True)
+			collection_births.update({"_id" : unitB["_id"]},{"$set":{"matched" : 1}}, upsert=False, multi=True)
+
+			collection_matches.insert({"unitA" : unitA,
+				   		   "unitB" : unitB,
+						   "max_subunits" : max_subunits,
+						   "subunits_created" : 0,
+						   "last_subunit_created" : 0})
+			print("units matched")
+		else:
+			print("no units to match")
 	
 					
 
 
 
-
-#		collection.insert({"unitA_id" : "test",
-#				   "unitA_firstname" : "test",
-#				   "unitA_familyname" : "test",
-#				   "b_firstname" : "test"})
 
 
 
