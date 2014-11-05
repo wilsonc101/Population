bb242e39fa8284a907abb8dd3c7bfa8cbdd0709e
Chris <wilsonc101@gmail.com>
2014-11-05 12:35:09 +0000

    fix destroy issue

--- unit/destroy.py
+++ unit/destroy.py
@@ -3,16 +3,16 @@ import pymongo
 
 def Destroy(collection_births, collection_deaths, iteration):
 	# Check if any units are to be destroyed
-	if collection_births.find({"die" : iteration}).count() > 0:
-
+	if collection_births.find({"die" : {"$lte" : iteration}}).count() > 0:
 		# Add units to be destroyed to the deaths collection
-		collection_deaths.insert(collection_births.find({"die" : iteration}))
+		collection_deaths.insert(collection_births.find({"die" : {"$lte" : iteration}}))
 
 		# Count units before destruction
 		pre_destroy = collection_births.count()
 
+
 		# Destroy units
-		collection_births.remove({"die" : iteration})
+		collection_births.remove({"die" : {"$lte" : iteration}})
 
 		post_destroy = collection_births.count()
 
